42ba509268af82334957361bc9ed64100605067f
Carlos Avila Aguilar <krloz1003@gmail.com>
2019-06-20 21:29:49 -0500

    Cargando routing y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrutaComponent } from './fruta/fruta.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrutaComponent,
-    EmpleadoComponent
+    EmpleadoComponent,
+    ContactoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
