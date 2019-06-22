b7d43ce406d1a1e9c7014411b12e59c866bfd8a7
Carlos Avila Aguilar <krloz1003@gmail.com>
2019-06-21 21:10:31 -0500

    36. Crear y utilizar un servicio: creando metodos y pasando parametros

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+// importando el servicio
 import { RopaService } from '../services/ropa.service';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [RopaService]
+  providers: [RopaService] // Agregamos el provider
 })
 export class HomeComponent implements OnInit {
   public titulo = "Pagina principal";
   
   constructor(
+    // Creamos la instancia
   	private _ropaService: RopaService
-  	) { }
+  ) { }
 
   ngOnInit() {
-  	console.log(this._ropaService.prueba('camisa nike'));
+  	// Lalamamos al método prueba
+    console.log(this._ropaService.prueba('camisa nike'));
 
   }
 
--- src/app/services/ropa.service.ts
+++ src/app/services/ropa.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class RopaService {
-	public nombre_prenda = 'Pantalones vaqueros';
+	public nombre_prenda = 'Pantalones de vestir';
 
 	prueba(nombre_prenda) {
 		return nombre_prenda;
